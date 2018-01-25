bcc70e453a4fb3d2db521b81fe0b50404cf10d95
IT5706021632162 <5706021632162@fitm.kmutnb.ac.th>
2018-01-26 03:02:47 +0700

    1-7

--- src/components/TodosFooter.vue
+++ src/components/TodosFooter.vue
@@ -1,16 +1,40 @@
 <template>
   <div>
-    <p class="is-pulled-left">3 items left</p>
+    <p class="is-pulled-left"  v-if="visibility === 'all'">{{count}} items left</p>
+    <p class="is-pulled-left"  v-if="visibility === 'active'">{{count}} items left</p>
+    <p class="is-pulled-left"  v-if="visibility === 'completed'" >{{count}} items left</p>
+
     <visibility-input/>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import VisibilityInput from '@/components/VisibilityInput'
 
 export default {
   components: {
     VisibilityInput
+  },
+  data () {
+    return {
+      status: 'All'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'todos',
+      'visibility'
+    ]),
+    count () {
+      if (this.visibility === 'active') {
+        return this.todos.filter(todo => todo.completed === false).length
+      } else if (this.visibility === 'completed') {
+        return this.todos.filter(todo => todo.completed === true).length
+      } else if (this.visibility === 'all') {
+        return this.todos.length
+      }
+    }
   }
 }
 </script>
